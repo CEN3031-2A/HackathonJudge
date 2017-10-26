901f5365f014240f7afc1c5921ecae94cf74590b
Reid Mollway <reidmollway@gmail.com>
2017-10-26 16:55:10 -0400

    Made changes to allow dynamic addition / removal of sub-components

--- modules/hackathons/client/controllers/hackathons.client.controller.js
+++ modules/hackathons/client/controllers/hackathons.client.controller.js
@@ -17,6 +17,33 @@
     vm.form = {};
     vm.remove = remove;
     vm.save = save;
+    vm.create = create;
+    vm.addProjectToCategory = addProjectToCategory;
+    vm.addCriteriaToCategory = addCriteriaToCategory;
+    vm.removeProjectFromCategory = removeProjectFromCategory;
+    vm.removeCriteriaFromCategory = removeCriteriaFromCategory;
+    vm.addCategoryToHackathon = addCategoryToHackathon;
+    vm.removeCategoryFromHackathon = removeCategoryFromHackathon;
+
+    vm.months = [
+      {name: 'January', num: 1},
+      {name: 'February', num: 2},
+      {name: 'March', num: 3},
+      {name: 'April', num: 4},
+      {name: 'May', num: 5},
+      {name: 'June', num: 6},
+      {name: 'July', num: 7},
+      {name: 'August', num: 8},
+      {name: 'September', num: 9},
+      {name: 'October', num: 10},
+      {name: 'November', num: 11},
+      {name: 'December', num: 12}
+    ];
+
+    // vm.currentYear = parseInt(new Date().toJSON().substr(0,4));
+    // console.log("Current Year: ");
+    // console.log(vm.currentYear);
+    // console.log(typeof vm.currentYear);
 
     if ($stateParams.cat != null) {
       vm.catToUpdate = hackathon.category[$stateParams.cat];
@@ -28,6 +55,51 @@
       if ($window.confirm('Are you sure you want to delete?')) {
         vm.hackathon.$remove($state.go('hackathons.list'));
       }
+    }
+
+    // Create new Hackathon
+    function create() {
+      vm.hackathon = new Hackathon();
+    }
+
+    function addProjectToCategory() {
+      var newProj = {
+        name: '',
+        description: '',
+        link: ''
+      };
+      vm.catToUpdate.project[vm.catToUpdate.project.length] = newProj;
+    }
+
+    function addCriteriaToCategory() {
+      var newCriteria = {
+        name: '',
+        description: '',
+        input_type: ''
+      };
+      vm.catToUpdate.criteria[vm.catToUpdate.criteria.length] = newCriteria;
+    }
+
+    function addCategoryToHackathon() {
+      var newCategory = {
+        name: 'New Category',
+        description: ''
+      };
+      vm.hackathon.category[vm.hackathon.category.length] = newCategory;
+    }
+
+    function removeProjectFromCategory(project) {
+      vm.catToUpdate.project.splice(vm.catToUpdate.project.indexOf(project), 1);
+    }
+
+    function removeCriteriaFromCategory(criteria) {
+      vm.catToUpdate.criteria.splice(vm.catToUpdate.criteria.indexOf(criteria), 1);
+    }
+
+    function removeCategoryFromHackathon() {
+      vm.hackathon.category.splice(vm.hackathon.category.indexOf(vm.catToUpdate), 1);
+      vm.catToUpdate = null;
+      vm.save(true);
     }
 
     // Save Hackathon
