bf3b492e19b02ff8b1c9271200db4a880fb2d74f
Steven Y <stevenyildirim@gmail.com>
2017-10-27 16:33:48 -0400

    getting of dev and working judge page

---
+++ modules/hackathons/client/services/projects.client.service.js
@@ -0,0 +1,19 @@
+// Hackathons service used to communicate Hackathons REST endpoints
+(function () {
+    'use strict';
+  
+    angular
+      .module('hackathons')
+      .factory('ProjectsService', ProjectsService);
+  
+    ProjectsService.$inject = ['$resource'];
+  
+    function ProjectsService($resource) {
+      return $resource('api/hackathons', {}, {
+        query: {
+          method: 'GET', isArray:true
+        }
+      });
+    }
+  }());
+  
