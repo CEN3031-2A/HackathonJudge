6d2b577906213a45f6231f73bcc84c8756f4cc10
Reid Mollway <reidmollway@gmail.com>
2017-11-10 17:34:22 -0500

    Added form for vote submission

--- modules/hackathons/client/controllers/list-projects.client.controller.js
+++ modules/hackathons/client/controllers/list-projects.client.controller.js
@@ -5,12 +5,18 @@
       .module('hackathons')
       .controller('ProjectsListController', ProjectsListController);
 
-    ProjectsListController.$inject = ['ProjectsService'];
+    ProjectsListController.$inject = ['ProjectsService', 'Socket'];
 
-    function ProjectsListController(ProjectsService) {
+    function ProjectsListController(ProjectsService, Socket) {
       var vm = this;
 
       vm.hackathons = ProjectsService.query();
+
+      vm.blockchain = [];
+      vm.voteValues = [];
+      // vm.CurrentProject = null;
+      // vm.sendVote = sendVote;
+      vm.saveVote = saveVote;
 
       vm.scaleArray = [
         ['1'],
@@ -29,5 +35,49 @@
         ['1','2','3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14'],
         ['1','2','3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15']
       ];
+
+  // function saveVote() {
+    // vm.blockchain.push(project.tempVote);
+  // }
+
+      // init();
+      //
+      // function init() {
+      //   // Make sure the Socket is connected
+      //   if (!Socket.socket) {
+      //     Socket.connect();
+      //   }
+      //
+      //   // Add an event listener to the 'chatMessage' event
+      //   Socket.on('voteMessage', function (newBlock) {
+      //     vm.blockchain.push(newBlock);
+      //   });
+      //
+      //   // Remove the event listener when the controller instance is destroyed
+      //   $scope.$on('$destroy', function () {
+      //     Socket.removeListener('voteMessage');
+      //   });
+      // }
+
+      // Create a controller method for sending messages
+      function saveVote(projectName) {
+        // Create a new message object
+        var data = {
+          sender: 2,
+          recipient: projectName,
+          voteCriteria1: vm.tempVote[0],
+          voteCriteria2: vm.tempVote[1],
+          voteCriteria3: vm.tempVote[2],
+          voteCriteria4: vm.tempVote[3]
+        };
+
+        console.log(data);
+        // Emit a 'voteMessage' message event
+        Socket.emit('voteMessage', data);
+
+        // Clear the message text
+        // vm.voteValues = [];
+      }
+
     }
 }());
