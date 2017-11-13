bfd4eb9674670cb9d368362ab692946b0f27aeb1
Reid Mollway <reidmollway@gmail.com>
2017-11-13 12:55:55 -0500

    Added results controller as listener to voteMessage events

--- modules/hackathons/client/controllers/results-hackathons.client.controller.js
+++ modules/hackathons/client/controllers/results-hackathons.client.controller.js
@@ -4,23 +4,24 @@
 
 angular.module("hackathons").controller("ResultsController", ResultsController);
 
-ResultsController.$inject = ['$scope', '$stateParams', '$state', '$window', 'Authentication', 'hackathonResolve'];
+ResultsController.$inject = ['$scope', '$stateParams', '$state', '$window', 'Authentication', 'hackathonResolve', 'Socket'];
 
-function ResultsController($scope, $stateParams, $state, $window, Authentication, hackathon) {
+function ResultsController($scope, $stateParams, $state, $window, Authentication, hackathon, Socket) {
   var vm = this;
   vm.authentication = Authentication;
   vm.hackathon = hackathon;
-  
-  // Graph variables 
+
+  // Graph variables
   $scope.labels = [];   // 2D array that holds x-axis labels for each graph -- e.g. $scope.labels[0] accesses labels for category 0
   $scope.series = [];   // 3D array that holds how many bars there will be per label (i.e. criteria) -- e.g. $scope.series[0] accesses criteria for category 0
   $scope.data = [];     // 3D array that holds data points for each graph -- e.g. $scope.data[0] accesses data points for category 0
   $scope.options = [];  // 1D array that holds how each graph will be structured - only part that changes per option is the title
-  
+
   // Variables accessed by the results view
   vm.projects = [];   // 2D array which will hold the projects -- e.g. vm.projects[0] contains projects corresponding to category 0
 
-  
+  vm.blockchain = [];
+
   // Iterate over the categories to extract information for above variables
   for (var cat=0; cat < vm.hackathon.category.length; cat++) {
     // Variables which will be pushed into the respective $scope variables (series and data will be 2D arrays, labels is 1D, options is an object)
@@ -54,17 +55,17 @@ function ResultsController($scope, $stateParams, $state, $window, Authentication
 
     var projects = vm.hackathon.category[cat].project;  // Holds projects of given category
     var criteria = vm.hackathon.category[cat].criteria; // Holds criteria of given category
-    
+
     // Push project names into labels
     for (var i=0; i < projects.length; i++) {
-      labels.push(projects[i].name);  
+      labels.push(projects[i].name);
     }
 
     $scope.labels.push(labels); // Push 1D array of labels into $scope.labels (forming 2D array)
 
     // Push criteria into series
     for(var i=0; i < criteria.length; i++) {
-      series.push(criteria[i].name);  
+      series.push(criteria[i].name);
       data.push([]);                  // Push empty arrays to hold the votes later
     }
 
@@ -93,13 +94,35 @@ function ResultsController($scope, $stateParams, $state, $window, Authentication
         data[curr_criteria].push(vote_sum); // Push average into data
       }
 
-      projects[curr_project].vote_size = notes.length;  // Store how many votes a project has      
+      projects[curr_project].vote_size = notes.length;  // Store how many votes a project has
     }
 
-    $scope.data.push(data);     // Push 2D array of data corresponding to its category into $scope.data 
-    vm.projects.push(projects); // Push 1D array of projects corresponding to its category into vm.projects 
+    $scope.data.push(data);     // Push 2D array of data corresponding to its category into $scope.data
+    vm.projects.push(projects); // Push 1D array of projects corresponding to its category into vm.projects
   }
-  
+
+  init();
+
+  function init() {
+    // Make sure the Socket is connected
+    if (!Socket.socket) {
+      Socket.connect();
+    }
+
+    // Add an event listener to the 'chatMessage' event
+    Socket.on('voteMessage', function (newBlock) {
+      if(newBlock.type == 'vote')
+      {
+        vm.blockchain.push(newBlock);
+      }
+      console.log(newBlock);
+    });
+
+    // Remove the event listener when the controller instance is destroyed
+    $scope.$on('$destroy', function () {
+      Socket.removeListener('voteMessage');
+    });
+  }
 
   // Sample data for reference
   /*
